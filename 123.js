96408a28786a49b2999f152d428937875e04357e
ArthurPlohoy <37124110+ArthurPlohoy@users.noreply.github.com>
2018-03-17 02:21:47 +0300

    Задание 3

---
+++ 123.js
@@ -0,0 +1,12 @@
+//Задание 3
+var request = require('request');
+
+request.post(
+    'https://api.privatbank.ua/p24api/pubinfo?json&exchange&coursid=3',
+    { json: { key: 'value' } },
+    function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+            console.log(body)
+        }
+    }
+);
